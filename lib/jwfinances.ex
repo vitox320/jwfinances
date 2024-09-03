8ee5f18121b0f80c340a1c060aca3093b74ed3da
Joao Victor <joao.conceicao@ses.ba>
2024-09-03 17:31:30 -0300

    feat remodeling users module and account module. Now each user will available to create multiples month balances accounts with multiples payments

--- lib/jwfinances.ex
+++ lib/jwfinances.ex
@@ -3,20 +3,36 @@ defmodule Jwfinances do
   alias Jwfinances.Account
 
   def setup(user) do
-    user
-    |> Account.create()
+    %{user: user, accounts: []}
     |> set_state()
   end
 
-  def set_state(user_account) do
-    Agent.start_link(fn -> user_account end, name: __MODULE__)
+  def new_account(month, year, month_invoicing) do
+    current_state()
+    |> Map.get(:accounts)
+    |> fill_accounts(Account.create(month, year, month_invoicing))
+    |> add_account_state()
+  end
+
+  defp fill_accounts(accounts, account) do
+    [account | accounts]
+  end
+
+  defp add_account_state(accounts) do
+    current_state()
+    |> Map.put(:accounts, accounts)
+    |> update_state()
+  end
+
+  def set_state(state) do
+    Agent.start_link(fn -> state end, name: __MODULE__)
   end
 
   def current_state do
     Agent.get(__MODULE__, & &1)
   end
 
-  def update_state(user_account) do
-    Agent.update(__MODULE__, fn _ -> user_account end)
+  def update_state(state) do
+    Agent.update(__MODULE__, fn _ -> state end)
   end
 end
--- lib/payments.ex
+++ lib/payments.ex
@@ -1,22 +1,11 @@
 defmodule Jwfinances.Payments do
-  @fields [:name, :value, :date]
+  @fields [:name, :value]
   @enforce_keys @fields
   defstruct @fields
 
-  def create(name, value, date) do
-    %Jwfinances.Payments{name: name, value: value, date: setDate(date)}
+  def create(name, value) do
+    %Jwfinances.Payments{name: name, value: value}
   end
 
-  defp setDate(date) do
-    Date.from_iso8601(date)
-    |> validate_date()
-  end
 
-  defp validate_date({:ok, date}) do
-    {:ok, date |> Date.to_string()}
-  end
-
-  defp validate_date({:error, _}) do
-    {:error, "invalid date"}
-  end
 end
