b0ee1621de3d787ef7ac65f41d21ccd3af7d3666
João Victor <vitox320@gmail.com>
2024-09-03 17:31:30 -0300

    feat remodeling users module and account module. Now each user will available to create multiples month balances accounts with multiples payments

--- lib/account.ex
+++ lib/account.ex
@@ -1,10 +1,10 @@
 defmodule Jwfinances.Account do
-  @fields [:user, :account_balance]
-  @enforce_keys [:account_balance, :user]
+  @fields [:month, :year, :month_invoicing ,account_balance: 0, payments: [] ]
+  @enforce_keys [ :month, :year, :month_invoicing ]
   defstruct @fields
 
-  def create(user) do
-    %Jwfinances.Account{user: user, account_balance: 0}
+  def create(month,year,month_invoicing) do
+    %Jwfinances.Account{ month: month, year: year, month_invoicing: month_invoicing}
   end
 
   def deposit(value) when value < 0, do: {:error, "Invalid value"}
@@ -30,4 +30,5 @@ defmodule Jwfinances.Account do
     |> Map.put(:account_balance, value)
     |> Jwfinances.update_state()
   end
+
 end
